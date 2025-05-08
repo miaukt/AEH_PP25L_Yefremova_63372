ad0dfb91ecf918b61c1e2b4516d82cf9f82c2f91
Lab1 <yefrem6817912_aehit@students.vizja.pl>
2025-05-08 20:34:28 +0200

    Pierwszy zapis do repozytoriumLab8

--- src/pl/pp/Person.java
+++ src/pl/pp/Person.java
@@ -1,46 +1,57 @@
 package pl.pp;
 
 public class Person {
-    public String forename;
-    public String surname;
-    public int age;
-    public String adres;
-    public int rok_urodzenia;
+    // pola (atrybuty) klasy określającej obiekt "Person" (jego parametry/cechy)
+    private String forename; // pole przechowujące cechę imię
+    private String surname; // pole przechowujące cechę nazwisko
+    private int age; // pole przechowujące cechę wiek
 
+    /**
+     * Konstruktor domyślny - element inicjalizujący pola obiektu podczas jego tworzenia
+     * (jeśli sami nie podamy żadnych parametrów)
+     */
     public Person() {
     }
 
-    public Person(String initForename, String initSurname, int initAge) {
-        forename = initForename;
-        surname = initSurname;
-        age = initAge;
+    /**
+     * Konstruktor z trzema parametrami, które możemy podać przy jego tworzeniu,
+     * wykorzystanie "this" pozwala m.in. na skorzystanie z tych samych nazw
+     * pól i zmiennych
+     */
+    public Person(String forename, String surname, int age) {
+        this.forename = forename;
+        this.surname = surname;
+        this.age = age;
     }
 
-
+    // metody klasy określające akcje możliwe do wykonania przez obiekt
     public void hiToAll(){
         System.out.println("Nazywam się " + forename + " " + surname + ". " + "Mam " + age + " lat.");
     }
 
-    public int growOld(int i){
-        if (i == 18) {
-            age = age + 10;
-        } else {
-            age = age + i;
-        }
-        if (age < 0) age = 0;
-        return age;
-    }
-    public int beYounger(){
-        age = age - 1;
-        if (age < 0) age = 0;
+    // przykładowa metoda growOld, która zwiększa dany wiek obiektu o jeden za każdym jej wywołaniem
+    public int growOld(int age){
+        this.age += age;
         return age;
     }
 
-    public String getName(){
+    // tzw. "gettery" i "settery" pól prywatnych
+    public String getForename(){
         return forename;
     }
-
-    public void setName(String nameToSet){
-        forename = nameToSet;
+    public void setForename(String forename){
+        this.forename = forename;
+    }
+    public String getSurname(){
+        return surname;
+    }
+    public void setSurname(String surname){
+        this.surname = surname;
+    }
+    public int getAge(){
+        return age;
+    }
+    public void setAge(int age){
+        this.age = age;
     }
 }
